ec788833d17411c817ee91140b3487d0552009c0
shoubana p <shoubanap08@gmail.com>
2024-01-29 23:04:08 +0300

    last chenges,iam add localStorage

--- house.js
+++ house.js
@@ -3,20 +3,39 @@
 let kitchenInput = document.getElementById("kitchen-input");
 let addBtn = document.getElementById("add-btn");
 let kitchenItemsList = document.getElementById("kitchen-items-list");
+let kitchenInputData;
+let kitchenInputDataArray = [];
 //create an empty array
 let kitchenItemsListArray = [];
-//-----------
-// step : 2
-//  Add Kitchen Items
-function addKitchenItems() {
-  let kitchenInputData = kitchenInput.value;
+//
+// set localStorage
+function setLocalStorage() {
+  localStorage.setItem("kitchenInput", JSON.stringify(kitchenInputDataArray));
+}
 
-  if (kitchenInputData !== "") {
-    // Create a new list item
+//Get  datas from localStorage
+function getLocalstorge() {
+  if (localStorage.getItem("kitchenInput")) {
+    kitchenInputDataArray = JSON.parse(localStorage.getItem("kitchenInput"));
+    console.log(kitchenInputDataArray);
+    buildUI();
+    // console.log("Data found");
+  }
+}
+
+function buildUI() {
+  kitchenItemsList.textContent = "";
+  kitchenInputDataArray.forEach((item) => {
+    console.log(item);
+
+    // Create a new list item(DOM ELEMENT)
     let li = document.createElement("li");
-    li.innerText = kitchenInputData;
-    
-    
+
+    //or//
+    let spanEl = document.createElement("span");
+    li.appendChild(spanEl);
+    spanEl.innerText = item;
+
     //Add item in Array
     kitchenItemsListArray.push(kitchenInputData);
 
@@ -30,21 +49,38 @@ function addKitchenItems() {
     kitchenInput.focus();
 
     // Log the updated array
-    console.log(kitchenItemsListArray);
+    // console.log(kitchenItemsListArray);
 
     //create trash button(delete button)
     let trashBtn = document.createElement("i");
     trashBtn.classList.add("fas", "fa-trash");
     li.appendChild(trashBtn);
 
-    console.log(trashBtn);
+    // console.log(trashBtn);
 
     //create a EditButton//
     let editBtn = document.createElement("i");
     editBtn.classList.add("fas", "fa-edit");
     li.appendChild(editBtn);
-  }
+  });
 }
+//-----------
+
+// step : 2
+//  Add Kitchen Items
+function addKitchenItems() {
+  kitchenInputData = kitchenInput.value;
+
+  kitchenInputDataArray.push(kitchenInputData);
+
+  console.log(kitchenInputDataArray);
+  // set localStorage//
+  setLocalStorage();
+
+  //get data from localStorage
+  getLocalstorge();
+}
+// step 3//
 // Delete item from kitchen list//
 function deleteKitchenItem(event) {
   console.log(event);
@@ -62,36 +98,24 @@ function deleteKitchenItem(event) {
 }
 
 // edit KitchenIem//
-function editKitchenItem(event){
+function editKitchenItem(event) {
   console.log(event.target.classList);
-  if(event.target.classList[1]==='fa-edit'){
-    let editedValue = prompt("Please Add New Text")
+  if (event.target.classList[1] === "fa-edit") {
+    let editedValue = prompt("Please Add New Text");
     console.log(editedValue);
     let item = event.target.parentElement;
     console.log(item);
-    
-     // Update the text content
-     item.innerText = editedValue;
+    let spanEl = item.querySelector("span");
+    console.log(spanEl);
 
-     //recreate and append the trash button
-
-     let trashBtn =document.createElement('i');
-     trashBtn.classList.add('fas','fa-trash')
-     item.appendChild(trashBtn)
-
-     let editBtn =document.createElement("i");
-     editBtn.classList.add('fas','fa-edit')
-     item.appendChild(editBtn)
-  
-   
-
+    // Update the text content
+    spanEl.innerText = editedValue;
   }
-
 }
 
-//Step :1-------------
+// //Step :1-------------
 
-// Combine the event listeners for delete and edit actions
+// // Combine the event listeners for delete and edit actions
 kitchenItemsList.addEventListener("click", function (event) {
   if (event.target.classList.contains("fa-trash")) {
     deleteKitchenItem(event);
@@ -110,13 +134,7 @@ kitchenInput.addEventListener("keypress", function (event) {
     addKitchenItems();
   }
 });
-
-
-
-console.log(kitchenItemsListArray);
-
-
-//     ...........or// trash btn and editbtn//
+getLocalstorge();
 
 //////////KITCHEN PROJECT///
 
@@ -131,16 +149,18 @@ console.log(kitchenItemsListArray);
 // function addKitchenItems() {
 //   let kitchenInputData = kitchenInput.value;
 
+//   // //set localStorage//
+//   // localStorage.setItem("kitchenInput",kitchenInputData);
+
+//   // //get data from localStorage
+//   //  kitchenInputData =localStorage.getItem(kitchenInput);
+//   // console.log(kitchenInputData);
+
 //   if (kitchenInputData !== "") {
-//     // Create a new list item
+//     // Create a new list item(DOM ELEMENT)
 //     let li = document.createElement("li");
-     
-//     //or//
-//     let spanEl =document.createElement('span');
-//     li.appendChild(spanEl);
-//     spanEl.innerText =kitchenInputData;
-    
-    
+//     li.innerText = kitchenInputData;
+
 //     //Add item in Array
 //     kitchenItemsListArray.push(kitchenInputData);
 
@@ -193,14 +213,19 @@ console.log(kitchenItemsListArray);
 //     console.log(editedValue);
 //     let item = event.target.parentElement;
 //     console.log(item);
-//     let spanEl = item.querySelector("span")
-//     console.log(spanEl);
-    
-//      // Update the text content
-//      spanEl.innerText = editedValue;
 
-  
-   
+//      // Update the text content
+//      item.innerText = editedValue;
+
+//      //recreate and append the trash button
+
+//      let trashBtn =document.createElement('i');
+//      trashBtn.classList.add('fas','fa-trash')
+//      item.appendChild(trashBtn)
+
+//      let editBtn =document.createElement("i");
+//      editBtn.classList.add('fas','fa-edit')
+//      item.appendChild(editBtn)
 
 //   }
 
@@ -228,21 +253,18 @@ console.log(kitchenItemsListArray);
 //   }
 // });
 
-
-
 // console.log(kitchenItemsListArray);
 
+//     ...........or// trash btn and editbtn//
 
 // local storage //
 // //set data to Loxalstorage//
 //  localStorage.setItem("name","shoubana");
 //  localStorage.setItem('passion',"dev")
 
-
 //  //get data from localstorage//
 //  let name =localStorage.getItem("name");
 //  console.log(name);
- 
- 
+
 //  let passion =localStorage.getItem("passion");
 //  console.log(passion);
